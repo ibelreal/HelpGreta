ade5eef2c4f13960661bd0637d49c113a3e2befe
Isabel del R <i.beldelreal@gmail.com>
2020-02-13 17:36:59 +0100

    Added basename for BrowserRouter

--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ import { BrowserRouter } from 'react-router-dom';
 // import './index.css';
 
 
-//basename="/HelpGreta"
-ReactDOM.render(<BrowserRouter  >
+ReactDOM.render(<BrowserRouter basename="/HelpGreta">
     <App />
 </BrowserRouter>, document.getElementById('root'));
